3435e57827503ee28cbd4ec91ba90d3cd0c5adf4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-05 22:22:35 +0100

    window hello world

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
-#include "libft/ft_io.h"
+#include <X11/X.h>
+#include <X11/keysym.h>
 #include "mlx.h"
 
+static int key_hook(int keycode, void* mlx);
+static int exit_hook(void* mlx);
+
 int main(void) {
-    ft_putstr_fd("hi\n", 1);
     void* mlx = mlx_init();
+    void* window = mlx_new_window(mlx, 600, 400, "miniRT");
+
+    mlx_hook(window, DestroyNotify, StructureNotifyMask, exit_hook, mlx);
+    mlx_key_hook(window, &key_hook, mlx);
+    mlx_loop(mlx);
+}
+
+static int key_hook(int keycode, void* mlx) {
+    if (keycode == XK_Escape)
+        mlx_loop_end(mlx);
+    return (0);
+}
+
+static int exit_hook(void* mlx) {
+    mlx_loop_end(mlx);
+    return (0);
 }
