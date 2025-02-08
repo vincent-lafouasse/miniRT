5756083260b5316b09b15c2c90b9ffaa1c48316a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-08 20:57:22 +0100

    seed error module

---
+++ src/error/error_repr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_repr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 20:54:19 by poss              #+#    #+#             */
+/*   Updated: 2025/02/08 20:56:22 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "t_error.h"
+
+#define NO_ERROR_REPR "No error"
+#define OOM_REPR "Out of memory"
+#define UNREACHABLE_REPR "Programming error"
+
+const char	*error_repr(t_error err)
+{
+	if (err == NO_ERROR)
+		return (NO_ERROR_REPR);
+	if (err == E_OOM)
+		return (OOM_REPR);
+	if (err == E_UNREACHABLE)
+		return (UNREACHABLE_REPR);
+	return (UNREACHABLE_REPR);
+}
---
+++ src/error/t_error.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_error.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 20:53:20 by poss              #+#    #+#             */
+/*   Updated: 2025/02/08 20:57:11 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef T_ERROR_H
+# define T_ERROR_H
+
+typedef enum e_error	t_error;
+enum	e_error
+{
+	NO_ERROR,
+	E_OOM,
+	E_UNREACHABLE,
+};
+
+const char	*error_repr(t_error err);
+
+#endif
