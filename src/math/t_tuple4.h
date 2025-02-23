98ddb27feb95603fe9c291576988dabc41c7e622
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-23 15:53:46 +0100

    sketch tuple4 interface

--- src/math/t_tuple4.h
+++ src/math/t_tuple4.h
@@ -6,3 +6,10 @@ typedef struct {
     double z;
     double w;
 } t_tuple4;
+
+t_tuple4 tuple4_new(double x, double y, double z, double w);
+t_tuple4 tuple4_add(const t_tuple4 *t1, const t_tuple4 *t2);
+t_tuple4 tuple4_subtract(const t_tuple4 *t1, const t_tuple4 *t2);
+t_tuple4 tuple4_negate(const t_tuple4 *t);
+t_tuple4 tuple4_multiply(double x, const t_tuple4 *t);
+int tuple4_compare(const t_tuple4 *t1, const t_tuple4 *t2);
