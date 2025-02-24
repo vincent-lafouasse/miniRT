3dfbd9daa841e6d6ad5399f5b915922f8e2d93b0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-24 17:38:30 +0100

    implement {vec3,point}_is_valid

---
+++ src/math/t_point3.c
@@ -0,0 +1,6 @@
+#include "t_point3.h"
+
+bool point_is_valid(t_point3 point)
+{
+    return (point.w == 1.0);
+}
---
+++ src/math/t_vec3.c
@@ -0,0 +1,6 @@
+#include "t_vec3.h"
+
+bool vec3_is_valid(t_vec3 vec)
+{
+    return (vec.w == 0.0);
+}
