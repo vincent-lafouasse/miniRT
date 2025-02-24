6aa27e3c81f1bafc8e1096029412e2510e6b6085
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-24 17:40:27 +0100

    implement tuple4 test

--- test/MathTest/math_test.cpp
+++ test/MathTest/math_test.cpp
@@ -1,13 +1,19 @@
 #include "gtest/gtest.h"
 
-extern "C"
-{
+extern "C" {
+#include "math/t_point3.h"
 #include "math/t_tuple4.h"
 #include "math/t_vec3.h"
-#include "math/t_point3.h"
 };
 
-TEST(Dummy, Dummy)
-{
-    SUCCEED();
+TEST(Tuple, W1_means_point) {
+    t_tuple4 a = tuple4_new(4.3, -4.2, 3.1, 1.0);
+
+    ASSERT_EQ(a.x, 4.3);
+    ASSERT_EQ(a.y, -4.2);
+    ASSERT_EQ(a.z, 3.1);
+    ASSERT_EQ(a.w, 1.0);
+
+    ASSERT_FALSE(vec3_is_valid((t_vec3)a));
+    ASSERT_TRUE(point_is_valid((t_point3)a));
 }
