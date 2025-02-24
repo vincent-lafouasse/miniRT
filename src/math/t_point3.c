e594fb7e97c136b5f6aba87ad909b9b0f4b912f3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-24 17:53:14 +0100

    implement some vec3 operations

--- src/math/t_point3.c
+++ src/math/t_point3.c
@@ -1,5 +1,9 @@
 #include "t_point3.h"
 
+t_point3 point_new(double x, double y, double z) {
+    return tuple4_new(x, y, z, 1.0);
+}
+
 bool point_is_valid(t_point3 point) {
     return (point.w == 1.0);
 }
