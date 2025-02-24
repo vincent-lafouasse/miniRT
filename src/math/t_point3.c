49b7375fc5659a3c2c45aaab748bc246cce8d166
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-24 17:40:50 +0100

    format

--- src/math/t_point3.c
+++ src/math/t_point3.c
@@ -1,6 +1,5 @@
 #include "t_point3.h"
 
-bool point_is_valid(t_point3 point)
-{
+bool point_is_valid(t_point3 point) {
     return (point.w == 1.0);
 }
--- src/math/t_vec3.c
+++ src/math/t_vec3.c
@@ -1,6 +1,5 @@
 #include "t_vec3.h"
 
-bool vec3_is_valid(t_vec3 vec)
-{
+bool vec3_is_valid(t_vec3 vec) {
     return (vec.w == 0.0);
 }
