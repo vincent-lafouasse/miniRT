243049570109fe7903d8d9e43abc91f8f8e5be73
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-24 17:45:19 +0100

    more tests

--- test/MathTest/math_test.cpp
+++ test/MathTest/math_test.cpp
@@ -17,3 +17,22 @@ TEST(Tuple, W1_means_point) {
     ASSERT_FALSE(vec3_is_valid((t_vec3)a));
     ASSERT_TRUE(point_is_valid((t_point3)a));
 }
+
+TEST(Tuple, W0_means_point) {
+    t_tuple4 a = tuple4_new(4.3, -4.2, 3.1, 0.0);
+
+    ASSERT_EQ(a.x, 4.3);
+    ASSERT_EQ(a.y, -4.2);
+    ASSERT_EQ(a.z, 3.1);
+    ASSERT_EQ(a.w, 0.0);
+
+    ASSERT_TRUE(vec3_is_valid((t_vec3)a));
+    ASSERT_FALSE(point_is_valid((t_point3)a));
+}
+
+TEST(Tuple, Points_have_W1) {
+    t_point3 actual = point_new(4, -4, 3);
+    t_tuple4 expected = tuple4_new(4.3, -4.2, 3.1, 1.0);
+
+    ASSERT_EQ(tuple4_compare(&actual, &expected), true);
+}
