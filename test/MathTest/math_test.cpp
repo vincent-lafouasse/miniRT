ec1ed213e6e33823a3e2730c6afc01554490cb79
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-26 00:30:49 +0100

    test double equality with gtest assertion ASSERT_DOUBLE_EQ

--- test/MathTest/math_test.cpp
+++ test/MathTest/math_test.cpp
@@ -9,10 +9,10 @@ extern "C" {
 TEST(Tuple, W1_means_point) {
     t_tuple4 a = tuple4_new(4.3, -4.2, 3.1, 1.0);
 
-    ASSERT_EQ(a.x, 4.3);
-    ASSERT_EQ(a.y, -4.2);
-    ASSERT_EQ(a.z, 3.1);
-    ASSERT_EQ(a.w, 1.0);
+    ASSERT_DOUBLE_EQ(a.x, 4.3);
+    ASSERT_DOUBLE_EQ(a.y, -4.2);
+    ASSERT_DOUBLE_EQ(a.z, 3.1);
+    ASSERT_DOUBLE_EQ(a.w, 1.0);
 
     ASSERT_FALSE(vec3_is_valid(a));
     ASSERT_TRUE(point_is_valid(a));
@@ -21,10 +21,10 @@ TEST(Tuple, W1_means_point) {
 TEST(Tuple, W0_means_point) {
     t_tuple4 a = tuple4_new(4.3, -4.2, 3.1, 0.0);
 
-    ASSERT_EQ(a.x, 4.3);
-    ASSERT_EQ(a.y, -4.2);
-    ASSERT_EQ(a.z, 3.1);
-    ASSERT_EQ(a.w, 0.0);
+    ASSERT_DOUBLE_EQ(a.x, 4.3);
+    ASSERT_DOUBLE_EQ(a.y, -4.2);
+    ASSERT_DOUBLE_EQ(a.z, 3.1);
+    ASSERT_DOUBLE_EQ(a.w, 0.0);
 
     ASSERT_TRUE(vec3_is_valid(a));
     ASSERT_FALSE(point_is_valid(a));
