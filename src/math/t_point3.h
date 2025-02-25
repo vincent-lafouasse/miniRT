d31752252da90d59dc3f7040f20bf1cf314e69f0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-25 21:27:15 +0100

    format

--- src/math/t_point3.h
+++ src/math/t_point3.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#include "t_tuple4.h"
 #include <stdbool.h>
 
-#include "t_tuple4.h"
+typedef t_tuple4	t_point3;
 
-typedef t_tuple4 t_point3;
-
-t_point3 point_new(double x, double y, double z);
-bool point_is_valid(t_point3 point);
+t_point3			point_new(double x, double y, double z);
+bool				point_is_valid(t_point3 point);
--- src/math/t_tuple4.h
+++ src/math/t_tuple4.h
@@ -1,15 +1,16 @@
 #pragma once
 
-typedef struct {
-    double x;
-    double y;
-    double z;
-    double w;
-} t_tuple4;
+typedef struct
+{
+	double	x;
+	double	y;
+	double	z;
+	double	w;
+}			t_tuple4;
 
-t_tuple4 tuple4_new(double x, double y, double z, double w);
-t_tuple4 tuple4_add(t_tuple4 t1, t_tuple4 t2);
-t_tuple4 tuple4_subtract(t_tuple4 t1, t_tuple4 t2);
-t_tuple4 tuple4_negate(t_tuple4 t);
-t_tuple4 tuple4_multiply(double s, t_tuple4 t);
-int tuple4_compare(t_tuple4 t1, t_tuple4 t2);
+t_tuple4	tuple4_new(double x, double y, double z, double w);
+t_tuple4	tuple4_add(t_tuple4 t1, t_tuple4 t2);
+t_tuple4	tuple4_subtract(t_tuple4 t1, t_tuple4 t2);
+t_tuple4	tuple4_negate(t_tuple4 t);
+t_tuple4	tuple4_multiply(double s, t_tuple4 t);
+int			tuple4_compare(t_tuple4 t1, t_tuple4 t2);
