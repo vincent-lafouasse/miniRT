3cad80db06f01461ee3d737e831a9bbf4057a121
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-26 00:24:04 +0100

    implement double_cmp

--- src/math/double_cmp.c
+++ src/math/double_cmp.c
@@ -6,3 +6,10 @@ bool double_eq(const double a, const double b)
 {
     return (fabs(a - b) < EPSILON);
 }
+
+int double_cmp(double a, double b)
+{
+    if (double_eq(a, b))
+        return 0;
+    return 2.0 * (a - b) / fabs(a - b);
+}
--- src/math/math.h
+++ src/math/math.h
@@ -5,3 +5,4 @@
 #define EPSILON 0.000001
 
 bool double_eq(double a, double b);
+int double_cmp(double a, double b);
