2b98131c22d75069e591676a5227caf5e7c911d6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-26 00:26:53 +0100

    implement better tuple4_compare using double_cmp

--- src/math/t_tuple4.c
+++ src/math/t_tuple4.c
@@ -1,5 +1,7 @@
 #include "t_tuple4.h"
 
+#include "math.h"
+
 t_tuple4	tuple4_new(double x, double y, double z, double w)
 {
 	return ((t_tuple4){x, y, z, w});
@@ -25,16 +27,18 @@ t_tuple4	tuple4_multiply(double s, t_tuple4 t)
 	return (tuple4_new(s * t.x, s * t.y, s * t.z, s * t.w));
 }
 
-// this is terrible
 int	tuple4_compare(t_tuple4 t1, t_tuple4 t2)
 {
-	if (t1.x != t2.x)
-		return (t1.x - t2.x);
-	if (t1.y != t2.y)
-		return (t1.y - t2.y);
-	if (t1.z != t2.z)
-		return (t1.z - t2.z);
-	if (t1.w != t2.w)
-		return (t1.w - t2.w);
-	return (0);
+    int cmp;
+
+	cmp = double_cmp(t1.x, t2.x);
+	if (cmp)
+		return (cmp);
+	cmp = double_cmp(t1.y, t2.y);
+	if (cmp)
+		return (cmp);
+	cmp = double_cmp(t1.z, t2.z);
+	if (cmp)
+		return (cmp);
+    return double_cmp(t1.w, t2.w);
 }
