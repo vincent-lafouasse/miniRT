a14f138f90a90a188b490655bf49d32cdd3b86e0
bitquence <imbitquence@gmail.com>
2025-03-11 23:47:01 +0100

    do not link mlx with tests

--- test/DummyTest/dummy_test.cpp
+++ test/DummyTest/dummy_test.cpp
@@ -2,13 +2,10 @@
 
 extern "C" {
 #include "libft.h"
-#include "mlx.h"
 };
 
 TEST(Dummy, Dummy) {
     int dummy = ft_strlen("420");
-    void* mlx = mlx_init();
     (void)dummy;
-    (void)mlx;
     SUCCEED();
 }
