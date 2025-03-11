db84eb90116204a00d862674ccc36a3924b52fab
bitquence <imbitquence@gmail.com>
2025-03-12 00:31:50 +0100

    init scene and camera types

---
+++ src/camera/t_camera.h
@@ -0,0 +1,9 @@
+#ifndef T_CAMERA_H
+# define T_CAMERA_H
+
+struct s_camera {
+
+};
+typedef struct s_camera	t_camera;
+
+#endif // T_CAMERA_H
---
+++ src/scene/t_scene.h
@@ -0,0 +1,9 @@
+#ifndef T_SCENE_H
+# define T_SCENE_H
+
+struct s_scene {
+
+};
+typedef struct s_scene	t_scene;
+
+#endif // T_SCENE_H
