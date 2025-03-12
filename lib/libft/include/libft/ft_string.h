ef554a60fd229e637c30d9a4e03e2b38e136a51b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-12 01:43:13 +0100

    implement convenience ft_split_len and ft_split_destroy

--- lib/libft/include/libft/ft_string.h
+++ lib/libft/include/libft/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 10:14:24 by poss              #+#    #+#             */
-/*   Updated: 2025/03/12 01:25:33 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/12 01:38:22 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	**ft_split_charset(char const *s, char const* charset);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 bool	ft_str_starts_with(const char *s, const char *pattern);
+
+char	**ft_split(char const *s, char c);
+char	**ft_split_charset(char const *s, char const* charset);
+int		ft_split_len(const char** split);
+void	ft_split_destroy(char** split);
 
 #endif
