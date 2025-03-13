efe8b89e9983e9d3913092dff3cc49cd26ff3b10
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-13 02:12:07 +0100

    set covalent radii

--- aux/molecule/src/main.rs
+++ aux/molecule/src/main.rs
@@ -1,7 +1,26 @@
 #[allow(unused)]
 #[allow(dead_code)]
-
 use std::rc::Rc;
+
+enum AtomKind {
+    Hydrogen,
+    Carbon,
+    Oxygen,
+    Nitrogen,
+}
+
+impl AtomKind {
+    // picometers
+    fn covalent_radius(&self) -> f32 {
+        use AtomKind::*;
+        match self {
+            Hydrogen => 31.0,
+            Carbon => 73.0, // varies by hybridization but this value should be fine
+            Oxygen => 66.0,
+            Nitrogen => 71.0,
+        }
+    }
+}
 
 enum Atom {
     Bigonal(BigonalAtom),
