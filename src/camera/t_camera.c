6703881992a8723b1460e9c4c9acf7ff5e1640bc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-17 17:26:52 +0100

    refactor

--- src/camera/t_camera.c
+++ src/camera/t_camera.c
@@ -3,10 +3,20 @@
 #include <float.h>
 #include <math.h>
 
-void construct_viewport(t_vec3 direction,
-                        double aspect_ratio,
-                        t_vec3* viewport_u_out,
-                        t_vec3* viewport_v_out) {
+typedef struct s_dimension {
+    double w;
+    double h;
+} t_dimension;
+
+typedef struct s_viewport {
+    t_vec3 u;
+    t_vec3 v;
+    t_vec3 delta_u;
+    t_vec3 delta_v;
+} t_viewport;
+
+t_viewport construct_viewport(t_vec3 direction,
+                        t_dimension screen) {
     // set "up" vaguely towards positive y
     // if colinear to e_y, set to e_x
     // direction really should be normalized for the colinearity check to work
@@ -22,8 +32,26 @@ void construct_viewport(t_vec3 direction,
     t_vec3 viewport_right = vec3_cross(direction, viewport_up);
     t_vec3 viewport_down = vec3_cross(direction, viewport_right);
 
-    *viewport_v_out = vec3_mul(2.0, viewport_down);
-    *viewport_u_out = vec3_mul(2.0 * aspect_ratio, viewport_right);
+    t_vec3 u =  vec3_mul(2.0 * screen.w / screen.w, viewport_right);
+    t_vec3 v = vec3_mul(2.0, viewport_down);
+
+    return (t_viewport){
+        .u = u,
+        .v = v,
+        .delta_u = vec3_mul(2.0 * screen.w / screen.h, viewport_right),
+        .delta_v = vec3_mul(2.0, viewport_down),
+    };
+}
+
+t_vec3 compute_pixel00(t_point3 camera_position, t_vec3 direction, double fov_deg, t_viewport vp) {
+    double fov_radians = M_PI * fov_deg / 180.0;
+    double focal_length =
+        0.5 * vec3_length(vp.u) / tan(fov_radians / 2.0);
+    t_point3 viewport_center = vec3_add(camera_position, vec3_mul(focal_length, direction));
+    t_point3 viewport_top_left = vec3_sub(
+        viewport_center, vec3_mul(0.5, vec3_add(vp.u, vp.v)));
+
+    return vec3_add(viewport_top_left, vec3_mul(0.5, vec3_add(vp.delta_u, vp.delta_v)));
 }
 
 t_camera camera_new(t_point3 position,
@@ -31,28 +59,15 @@ t_camera camera_new(t_point3 position,
                     double fov_deg,
                     size_t screen_width,
                     size_t screen_height) {
-    t_vec3 viewport_u;
-    t_vec3 viewport_v;
-    construct_viewport(direction, (double)screen_width / (double)screen_height,
-                       &viewport_u, &viewport_v);
-    t_vec3 delta_u = vec3_div(viewport_u, (double)screen_width);
-    t_vec3 delta_v = vec3_div(viewport_v, (double)screen_height);
+    t_viewport vp = construct_viewport(direction, (t_dimension){.w = screen_width, .h = screen_height});
 
-    double fov_radians = M_PI * fov_deg / 180.0;
-    double focal_length =
-        0.5 * vec3_length(viewport_u) / tan(fov_radians / 2.0);
-    t_point3 viewport_center = vec3_mul(focal_length, direction);
-    t_point3 viewport_top_left = vec3_sub(
-        viewport_center, vec3_mul(0.5, vec3_add(viewport_u, viewport_v)));
-
-    t_point3 pixel00 =
-        vec3_add(viewport_top_left, vec3_mul(0.5, vec3_add(delta_u, delta_v)));
+    t_vec3 pixel00 = compute_pixel00(position, direction, fov_deg, vp);
 
     return (t_camera){
         .position = position,
         .pixel00 = pixel00,
-        .delta_u = delta_u,
-        .delta_v = delta_v,
+        .delta_u = vp.delta_u,
+        .delta_v = vp.delta_v,
         .screen_width = screen_width,
         .screen_height = screen_height,
     };
