4656a588ab2d408f3575be1c622448fca0f35184
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-17 19:24:54 +0100

    inline

--- src/math/t_interval/t_interval.h
+++ src/math/t_interval/t_interval.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 19:11:20 by poss              #+#    #+#             */
-/*   Updated: 2025/03/17 19:12:57 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/17 19:24:50 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ double interval_size(t_interval);
 bool interval_contains(t_interval, double);
 bool interval_surrounds(t_interval, double);
 
-static t_interval interval_new(double min, double max) {
+static inline t_interval interval_new(double min, double max) {
     return (t_interval){.min = min, .max = max};
 }
 
-static t_interval empty_interval(void) {
+static inline t_interval empty_interval(void) {
     return (t_interval){.min = INFINITY, .max = -INFINITY};
 }
 
-static t_interval interval_R(void) {
+static inline t_interval interval_R(void) {
     return (t_interval){.min = -INFINITY, .max = INFINITY};
 }
 
-static t_interval interval_R_plus(void) {
+static inline t_interval interval_R_plus(void) {
     return (t_interval){.min = 0.0, .max = INFINITY};
 }
 
