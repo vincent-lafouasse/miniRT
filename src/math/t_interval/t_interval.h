9f7897a9a9dddfc6005608f4884b19c488b023d8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-17 19:10:58 +0100

    write interface for t_interval

--- src/math/t_interval/t_interval.h
+++ src/math/t_interval/t_interval.h
@@ -1,6 +1,8 @@
 #ifndef T_INTERVAL_H
 # define T_INTERVAL_H
 
+#include <math.h>
+
 struct s_interval {
 	double min;
 	double max;
@@ -8,5 +10,20 @@ struct s_interval {
 typedef s_interval t_interval;
 
 t_interval interval_new(double min, double max);
+double interval_size(t_interval);
+bool interval_contains(t_interval, double);
+bool interval_surrounds(t_interval, double);
+
+static t_interval empty_interval(void) {
+	return (t_interval){.min = INFINITY, .max = -INFINITY};
+}
+
+static t_interval interval_R(void) {
+	return (t_interval){.min = -INFINITY, .max = INFINITY};
+}
+
+static t_interval interval_R_plus(void) {
+	return (t_interval){.min = 0.0, .max = INFINITY};
+}
 
 #endif // T_INTERVAL_H
