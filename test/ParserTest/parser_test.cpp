26951b08328829d6e7756f270bd18f9dfd4a476d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-18 19:18:25 +0100

    sketch minimal parser input

--- test/ParserTest/parser_test.cpp
+++ test/ParserTest/parser_test.cpp
@@ -4,6 +4,11 @@ extern "C" {
 #include "parser/parse.h"
 };
 
+const char* minimal = "C 1,2,3 1,0,0 90\n"
+                      "A 0.69 4,5,6\n"
+                      "L 0.42 7,8,9\n"
+;
+
 TEST(Dummy, Dummy) {
     SUCCEED();
 }
