4bf5090703c56598f1e3ac232336f9c0545f63ba
bitquence <imbitquence@gmail.com>
2025-03-18 21:41:59 +0100

    change cylinder diameter to radius

--- src/scene/objects/t_cylinder.c
+++ src/scene/objects/t_cylinder.c
@@ -1,9 +1,9 @@
 #include "./t_cylinder.h"
 
-t_cylinder cylinder_new(t_point3 point, t_vec3 axis, double diameter, double height, t_rgb color)
+t_cylinder cylinder_new(t_point3 point, t_vec3 axis, double radius, double height, t_rgb color)
 {
 	t_cylinder cylinder;
 
-	cylinder = (t_cylinder){.point = point, .axis = axis, .diameter = diameter, .height = height, .color = color};
+	cylinder = (t_cylinder){.point = point, .axis = axis, .radius = radius, .height = height, .color = color};
 	return (cylinder);
 }
--- src/scene/objects/t_cylinder.h
+++ src/scene/objects/t_cylinder.h
@@ -7,13 +7,13 @@
 struct s_cylinder {
 	t_point3	point;
 	t_vec3		axis;
-	double		diameter;
+	double		radius;
 	double		height;
 	t_rgb		color;
 };
 typedef struct s_cylinder t_cylinder;
 
-t_cylinder cylinder_new(t_point3 point, t_vec3 axis, double diameter, double height, t_rgb color);
+t_cylinder cylinder_new(t_point3 point, t_vec3 axis, double radius, double height, t_rgb color);
 //t_hit_record cylinder_hit(t_camera cam, t_ray ray);
 
 #endif // T_CYLINDER_H
