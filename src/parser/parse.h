53c67725ed8200e9557e056af242ff7c1fe43b91
bitquence <imbitquence@gmail.com>
2025-03-18 21:52:01 +0100

    fix parse method prototype

--- src/parser/parse.h
+++ src/parser/parse.h
@@ -5,6 +5,6 @@
 # include "camera/t_camera.h"
 # include "scene/t_scene.h"
 
-t_error	parse(char *input, t_camera *cam_out, t_scene *scene_out);
+t_error	parse(const char *input, t_camera_specs *cam_out, t_scene *scene_out);
 
 #endif // PARSE_H
