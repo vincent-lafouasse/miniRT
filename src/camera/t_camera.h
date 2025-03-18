266c777ce090770fe01e0c75cd9d681a596c06f3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-18 16:10:45 +0100

    format

--- src/camera/t_camera.h
+++ src/camera/t_camera.h
@@ -1,23 +1,22 @@
 #ifndef T_CAMERA_H
-#define T_CAMERA_H
+# define T_CAMERA_H
 
-#include <stddef.h>
-#include "math/t_vec3/t_vec3.h"
+# include "math/t_vec3/t_vec3.h"
+# include <stddef.h>
 
-struct s_camera {
-    t_point3 position;
-    t_point3 pixel00;
-    t_vec3 delta_u;
-    t_vec3 delta_v;
-    size_t screen_width;
-    size_t screen_height;
+struct	s_camera
+{
+	t_point3	position;
+	t_point3	pixel00;
+	t_vec3		delta_u;
+	t_vec3		delta_v;
+	size_t		screen_width;
+	size_t		screen_height;
 };
-typedef struct s_camera t_camera;
+typedef struct s_camera	t_camera;
 
-t_camera camera_new(t_point3 position,
-                    t_vec3 direction,
-                    double fov_deg,
-                    size_t screen_width,
-                    size_t screen_height);
+t_camera	camera_new(t_point3 position, t_vec3 direction,
+				double fov_deg, size_t screen_width,
+				size_t screen_height);
 
-#endif  // T_CAMERA_H
+#endif // T_CAMERA_H
