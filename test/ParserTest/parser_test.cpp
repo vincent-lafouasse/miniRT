66bd782c7aba656c9a7f0b5f854c0c03c1a6d51b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-18 19:32:22 +0100

    sketch parser test

--- test/ParserTest/parser_test.cpp
+++ test/ParserTest/parser_test.cpp
@@ -4,11 +4,24 @@ extern "C" {
 #include "parser/parse.h"
 };
 
-const char* minimal = "C 1,2,3 1,0,0 90\n"
-                      "A 0.69 4,5,6\n"
-                      "L 0.42 7,8,9\n"
-;
+TEST(Parser, Minimal) {
+    const char* minimal = "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+    ;
 
-TEST(Dummy, Dummy) {
-    SUCCEED();
+    t_vec3 expected_camera_position = vec3_new(1, 2, 3);
+    t_vec3 expected_camera_orientation = vec3_new(1, 0, 0);
+    std::size_t expected_fov_degrees = 90;
+
+    double expected_ambient_intensity = 0.69;
+    t_vec3 expected_ambient_color = vec3_new(4, 5, 6);
+
+    t_vec3 expected_pointlight_position = vec3_new(7, 8, 9);
+    double expected_pointlight_intensity = 0.69;
+    t_vec3 expected_pointlight_color = vec3_new(10, 11, 12);
+
+    t_camera camera;
+    t_scene scene;
+
 }
