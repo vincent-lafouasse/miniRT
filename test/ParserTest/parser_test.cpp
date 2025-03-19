5fbaad80805b375da9832c0e68b22a4290c51b43
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-19 19:33:02 +0100

    refactor using convenience custom assertions

--- test/ParserTest/parser_test.cpp
+++ test/ParserTest/parser_test.cpp
@@ -12,22 +12,47 @@ extern "C" {
     EXPECT_DOUBLE_EQ(a.y, b.y); \
     EXPECT_DOUBLE_EQ(a.z, b.z);
 
+#define EXPECT_CAM_EQ(a, b) \
+    EXPECT_VEC3_EQ(a.position, b.position); \
+    EXPECT_VEC3_EQ(a.direction, b.direction); \
+    EXPECT_DOUBLE_EQ(a.fov_deg, b.fov_deg);
+
+#define EXPECT_AMBIENT_EQ(a, b) \
+    EXPECT_VEC3_EQ(a.color, b.color); \
+    EXPECT_DOUBLE_EQ(a.intensity, b.intensity);
+
+#define EXPECT_POINT_LIGHT_EQ(a, b) \
+    EXPECT_VEC3_EQ(a.coordinates, b.coordinates); \
+    EXPECT_VEC3_EQ(a.color, b.color); \
+    EXPECT_DOUBLE_EQ(a.intensity, b.intensity)
+
+#define EXPECT_SPHERE_EQ(a, b) \
+    EXPECT_VEC3_EQ(a.origin, b.origin); \
+    EXPECT_VEC3_EQ(a.color, b.color); \
+    EXPECT_DOUBLE_EQ(a.radius, b.radius)
+
 TEST(Parser, BasicSceneWithoutHittablesWorks) {
     const char* input =
         "C 1,2,3 1,0,0 90\n"
         "A 0.69 4,5,6\n"
         "L 7,8,9 0.42 10,11,12\n";
 
-    t_point3 expected_camera_position = vec3_new(1, 2, 3);
-    t_vec3 expected_camera_direction = vec3_new(1, 0, 0);
-    double expected_fov_degrees = 90.0;
+    t_camera_specs expected_camera = (t_camera_specs){
+        .position = vec3_new(1, 2, 3),
+        .direction = vec3_new(1, 0, 0),
+        .fov_deg = 90.0,
+    };
 
-    double expected_ambient_intensity = 0.69;
-    t_rgb expected_ambient_color = rgb_from_elements(4, 5, 6);
+    t_ambient_light expected_ambient_light = (t_ambient_light){
+        .intensity = 0.69,
+        .color = rgb_from_elements(4, 5, 6),
+    };
 
-    t_point3 expected_point_light_coordinates = vec3_new(7, 8, 9);
-    double expected_point_light_intensity = 0.42;
-    t_rgb expected_point_light_color = rgb_from_elements(10, 11, 12);
+    t_point_light expected_point_light = (t_point_light){
+        .coordinates = vec3_new(7, 8, 9),
+        .intensity = 0.42,
+        .color = rgb_from_elements(10, 11, 12),
+    };
 
     t_camera_specs camera;
     t_scene scene;
@@ -35,16 +60,9 @@ TEST(Parser, BasicSceneWithoutHittablesWorks) {
     t_error err = parse(input, &camera, &scene);
     ASSERT_EQ(err, NO_ERROR) << std::format("received error from parse method: {}", error_repr(err));
 
-    EXPECT_VEC3_EQ(camera.position, expected_camera_position);
-    EXPECT_DOUBLE_EQ(camera.fov_deg, expected_fov_degrees);
-    EXPECT_VEC3_EQ(camera.direction, expected_camera_direction);
-
-    EXPECT_DOUBLE_EQ(scene.ambient_light.intensity, expected_ambient_intensity);
-    EXPECT_VEC3_EQ(scene.ambient_light.color, expected_ambient_color);
-
-    EXPECT_VEC3_EQ(scene.point_light.coordinates, expected_point_light_coordinates);
-    EXPECT_DOUBLE_EQ(scene.point_light.intensity, expected_point_light_intensity);
-    EXPECT_VEC3_EQ(scene.point_light.color, expected_point_light_color);
+    EXPECT_CAM_EQ(camera, expected_camera);
+    EXPECT_AMBIENT_EQ(scene.ambient_light, expected_ambient_light);
+    EXPECT_POINT_LIGHT_EQ(scene.point_light, expected_point_light);
 
     EXPECT_EQ(scene.objects->len, 0);
 }
@@ -56,20 +74,28 @@ TEST(Parser, BasicSceneWithOneSphereWorks) {
         "L 7,8,9 0.42 10,11,12\n"
         "sp 55,55,100  6    255,0,127\n";
 
-    t_point3 expected_camera_position = vec3_new(1, 2, 3);
-    t_vec3 expected_camera_direction = vec3_new(1, 0, 0);
-    double expected_fov_deg = 90.0;
+    t_camera_specs expected_camera = (t_camera_specs){
+        .position = vec3_new(1, 2, 3),
+        .direction = vec3_new(1, 0, 0),
+        .fov_deg = 90.0,
+    };
 
-    double expected_ambient_intensity = 0.69;
-    t_rgb expected_ambient_color = rgb_from_elements(4, 5, 6);
+    t_ambient_light expected_ambient_light = (t_ambient_light){
+        .intensity = 0.69,
+        .color = rgb_from_elements(4, 5, 6),
+    };
 
-    t_point3 expected_point_light_coordinates = vec3_new(7, 8, 9);
-    double expected_point_light_intensity = 0.42;
-    t_rgb expected_point_light_color = rgb_from_elements(10, 11, 12);
+    t_point_light expected_point_light = (t_point_light){
+        .coordinates = vec3_new(7, 8, 9),
+        .intensity = 0.42,
+        .color = rgb_from_elements(10, 11, 12),
+    };
 
-    t_point3 expected_sphere_origin = vec3_new(55, 55, 100);
-    double expected_sphere_diameter = 6.0;
-    t_rgb expected_sphere_color = rgb_from_elements(255, 0, 127);
+    t_sphere expected_sphere = (t_sphere){
+        .origin = vec3_new(55, 55, 100),
+        .radius = 6.0 / 2.0,
+        .color = rgb_from_elements(255, 0, 127),
+    };
 
     t_camera_specs camera;
     t_scene scene;
@@ -77,23 +103,13 @@ TEST(Parser, BasicSceneWithOneSphereWorks) {
     t_error err = parse(input, &camera, &scene);
     ASSERT_EQ(err, NO_ERROR) << std::format("received error from parse method: {}", error_repr(err));
 
-    EXPECT_VEC3_EQ(camera.position, expected_camera_position);
-    EXPECT_DOUBLE_EQ(camera.fov_deg, expected_fov_deg);
-    EXPECT_VEC3_EQ(camera.direction, expected_camera_direction);
-
-    EXPECT_DOUBLE_EQ(scene.ambient_light.intensity, expected_ambient_intensity);
-    EXPECT_VEC3_EQ(scene.ambient_light.color, expected_ambient_color);
-
-    EXPECT_VEC3_EQ(scene.point_light.coordinates, expected_point_light_coordinates);
-    EXPECT_DOUBLE_EQ(scene.point_light.intensity, expected_point_light_intensity);
-    EXPECT_VEC3_EQ(scene.point_light.color, expected_point_light_color);
+    EXPECT_CAM_EQ(camera, expected_camera);
+    EXPECT_AMBIENT_EQ(scene.ambient_light, expected_ambient_light);
+    EXPECT_POINT_LIGHT_EQ(scene.point_light, expected_point_light);
 
     ASSERT_EQ(scene.objects->len, 1);
 
     ASSERT_EQ(scene.objects->data[0].type, HITTABLE_SPHERE);
     t_sphere sphere = scene.objects->data[0].sphere;
-
-    EXPECT_VEC3_EQ(sphere.origin, expected_sphere_origin);
-    EXPECT_DOUBLE_EQ(sphere.radius, expected_sphere_diameter / 2.0);
-    EXPECT_VEC3_EQ(sphere.color, expected_sphere_color);
+    EXPECT_SPHERE_EQ(sphere, expected_sphere);
 }
