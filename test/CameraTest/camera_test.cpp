32e4d73ac7612a282fb9f109ab11df98515bc0c2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-19 20:21:46 +0100

    more tests

--- test/CameraTest/camera_test.cpp
+++ test/CameraTest/camera_test.cpp
@@ -27,6 +27,10 @@ static void log_camera(t_camera c, const char* name);
 [[maybe_unused]]
 static void log_vector(t_vec3 v, const char* name);
 
+static double rad_to_deg(double radians) {
+    return radians * 180.0 / M_PI;
+}
+
 TEST(Camera, AlongOz_OneByOne) {
     size_t h = 1;
     size_t w = 1;
@@ -41,7 +45,7 @@ TEST(Camera, AlongOz_OneByOne) {
     // tan fov/2 = width / 2*focal_length
     double target_focal_length = 1.0;
     double fov_rad = 2.0 * std::atan(0.5 * viewport_w / target_focal_length);
-    double fov_deg = fov_rad * 180.0 / M_PI;
+    double fov_deg = rad_to_deg(fov_rad);
 
     Vector expected_viewport_u = Vector::E_X() * viewport_w;
     Vector expected_viewport_v = -Vector::E_Y() * viewport_h;
@@ -65,6 +69,52 @@ TEST(Camera, AlongOz_OneByOne) {
     t_camera actual = camera_new(specs, w, h);
 
     EXPECT_CAM_EQ(expected, actual);
+}
+
+TEST(Camera, AlongOz_TwoByTwo) {
+    size_t h = 2;
+    size_t w = 2;
+
+    double aspect_ratio = static_cast<double>(w) / static_cast<double>(h);
+    double viewport_h = 2.0;
+    double viewport_w = 2.0 * aspect_ratio;
+
+    Vector origin = Vector::Origin();
+    Vector orientation = -Vector::E_Z();
+
+    // tan fov/2 = width / 2*focal_length
+    double target_focal_length = 1.0;
+    double fov_rad = 2.0 * std::atan(0.5 * viewport_w / target_focal_length);
+    double fov_deg = rad_to_deg(fov_rad);
+
+    Vector expected_viewport_u = Vector::E_X() * viewport_w;
+    Vector expected_viewport_v = -Vector::E_Y() * viewport_h;
+
+    Vector expected_delta_u = expected_viewport_u / static_cast<double>(w);
+    Vector expected_delta_v = expected_viewport_v / static_cast<double>(h);
+
+    Vector expected_viewport_center = origin + orientation;
+    Vector expected_viewport_top_left = expected_viewport_center - (expected_viewport_u + expected_viewport_v) * 0.5;
+    Vector expected_pixel00 = expected_viewport_top_left + (expected_delta_u + expected_delta_v) * 0.5;
+
+    t_camera expected = (t_camera){
+        .position = origin.get(),
+        .pixel00 = expected_pixel00.get(),
+        .delta_u = expected_delta_u.get(),
+        .delta_v = expected_delta_v.get(),
+        .screen_width = w,
+        .screen_height = h,
+    };
+    t_camera_specs specs = (t_camera_specs){
+        .position = origin.get(),
+        .direction = orientation.get(),
+        .fov_deg = fov_deg,
+    };
+    t_camera actual = camera_new(specs, w, h);
+
+    EXPECT_CAM_EQ(expected, actual);
+    log_camera(expected, "expected");
+    log_camera(actual, "actual");
 }
 
 [[maybe_unused]]
