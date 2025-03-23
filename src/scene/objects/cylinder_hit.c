011d47d3c53c42241a07836ab014973d6fd2675f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-23 16:42:18 +0100

    dummy cylinder_hit

---
+++ src/scene/objects/cylinder_hit.c
@@ -0,0 +1,15 @@
+#include "./t_plane.h"
+
+#include "./t_hittable.h"
+
+#include "math/t_interval/t_interval.h"
+#include "math/t_vec3/t_vec3.h"
+#include "ray/t_ray.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+bool cylinder_hit(t_cylinder cylinder, t_interval range, t_ray ray, t_hit_record *rec)
+{
+	return (false);
+}
--- src/scene/objects/object_hit.c
+++ src/scene/objects/object_hit.c
@@ -10,6 +10,7 @@
 
 bool sphere_hit(t_sphere sphere, t_interval range, t_ray ray, t_hit_record *rec);
 bool plane_hit(t_plane plane, t_interval range, t_ray ray, t_hit_record *rec);
+bool cylinder_hit(t_cylinder cylinder, t_interval range, t_ray ray, t_hit_record *rec);
 
 bool object_hit(const t_hittable *object, t_interval range, t_ray ray, t_hit_record *rec_out)
 {
@@ -19,6 +20,8 @@ bool object_hit(const t_hittable *object, t_interval range, t_ray ray, t_hit_rec
 		has_hit = sphere_hit(object->sphere, range, ray, rec_out);
 	else if (object->type == HITTABLE_PLANE)
 		has_hit = plane_hit(object->plane, range, ray, rec_out);
+	else if (object->type == HITTABLE_CYLINDER)
+		has_hit = cylinder_hit(object->cylinder, range, ray, rec_out);
 	else
 		abort(); // other shapes are unimplemented
 	if (has_hit)
