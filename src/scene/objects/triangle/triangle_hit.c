2531c6b235ed8fcf568d44e15072aeb15cee97a0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-26 21:37:04 +0100

    sketch triangle intersection

--- src/scene/objects/triangle/triangle_hit.c
+++ src/scene/objects/triangle/triangle_hit.c
@@ -7,5 +7,11 @@
 bool double_eq(double, double);
 
 bool triangle_hit(t_triangle triangle, t_interval range, t_ray ray, t_hit_record *rec) {
+	t_vec3 edge1 = vec3_sub(triangle.a, triangle.b);
+	t_vec3 edge2 = vec3_sub(triangle.a, triangle.c);
+	t_vec3 ray_cross_e2 = vec3_cross(ray.direction, edge2);
+	double det = vec3_dot(ray_cross_e2, edge1);
+	if (double_eq(0.0, det))
+		return false; // parallel
 	return false;
 }
