c54ad71464840c86f89f5b1b4510bdc1e64db45f
Jonathan <imbitquence@gmail.com>
2025-03-27 18:14:23 +0100

    implement failing parser tests

--- test/ParserTest/parser_test.cpp
+++ test/ParserTest/parser_test.cpp
@@ -113,3 +113,106 @@ TEST(Parser, BasicSceneWithOneSphereWorks) {
     t_sphere sphere = scene.objects->data[0].sphere;
     EXPECT_SPHERE_EQ(sphere, expected_sphere);
 }
+
+TEST(Parser, UnknownElementFails) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+        "sp 55,55,100  6    255,0,127\n"
+        "co 55,55,100  6    255,0,127\n";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_UNKNOWN_ELEMENT) << "received wrong error from parse method" << error_repr(err);
+}
+
+TEST(Parser, ExtraElementFieldsFails) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+        "sp 55,55,100  6    255,0,127\n"
+        "sp 55,55,100  6 20    255,0,127\n";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_MALFORMATTED_ELEMENT) << "received wrong error from parse method" << error_repr(err);
+}
+
+TEST(Parser, EmptyInputFails) {
+    const char* input = "";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+}
+
+TEST(Parser, ManyCamerasDoesNotWork) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+        "C 1,2,3 1,0,0 90\n"
+        "sp 55,55,100  6    255,0,127\n";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_MUST_HAVE_ONE_CAMERA) << "received wrong error from parse method" << error_repr(err);
+}
+
+TEST(Parser, IllegalPointLightFails) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 1.42 10,11,12\n"
+        "sp 55,55,100  6    255,0,127\n";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_OUT_OF_RANGE) << "received wrong error from parse method" << error_repr(err);
+}
+
+TEST(Parser, IllegalSphereObjectFails) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+        "sp 55,55,100  -42    255,0,127\n";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_OUT_OF_RANGE) << "received wrong error from parse method" << error_repr(err);
+}
+
+TEST(Parser, IllegalPlaneObjectFails) {
+    const char* input =
+        "C 1,2,3 1,0,0 90\n"
+        "A 0.69 4,5,6\n"
+        "L 7,8,9 0.42 10,11,12\n"
+        "pl	0,0,0			0,0,0			64,64,64";
+
+    t_camera_specs camera;
+    t_scene scene;
+
+    t_error err = parse(input, &camera, &scene);
+    EXPECT_NE(err, NO_ERROR) << "expected scene fail" << error_repr(err);
+    ASSERT_EQ(err, E_UNEXPECTED_NULL_VECTOR) << "received wrong error from parse method" << error_repr(err);
+}
