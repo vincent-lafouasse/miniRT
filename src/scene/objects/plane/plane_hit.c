ff2d0e2388e595f85fad9415b5550a7b4307ba72
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-27 19:57:18 +0100

    format

--- src/scene/objects/plane/plane_hit.c
+++ src/scene/objects/plane/plane_hit.c
@@ -1,54 +1,72 @@
-#include "./t_plane.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane_hit.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 19:53:15 by poss              #+#    #+#             */
+/*   Updated: 2025/03/27 19:56:21 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../t_hittable.h"
-
+#include "./t_plane.h"
 #include "math/t_interval/t_interval.h"
 #include "math/t_vec3/t_vec3.h"
 #include "ray/t_ray.h"
-
 #include <stdbool.h>
 #include <stddef.h>
 
-static bool parallel_ray_is_coplanar(t_plane plane, t_ray ray);
-static bool inside_of_plane(t_plane plane, t_ray ray, t_hit_record *out);
+static bool			parallel_ray_is_coplanar(t_plane plane, t_ray ray);
+static bool			inside_of_plane(t_plane plane, t_ray ray,
+						t_hit_record *out);
+static t_hit_record	construct_hit_record(double t, t_ray ray, t_plane plane);
 
-bool plane_hit(t_plane plane, t_interval range, t_ray ray, t_hit_record *rec)
+bool	plane_hit(t_plane plane, t_interval range, t_ray ray, t_hit_record *rec)
 {
+	t_vec3	camera_to_plane;
+	double	t;
+
 	if (vec3_dot(ray.direction, plane.normal) == 0.0)
 	{
 		if (parallel_ray_is_coplanar(plane, ray))
 			return (inside_of_plane(plane, ray, rec));
 		else
-			return false;
+			return (false);
 	}
-	
-	t_vec3 camera_to_plane = vec3_sub(plane.origin, ray.origin);
-	double t = vec3_dot(camera_to_plane, plane.normal) / vec3_dot(ray.direction, plane.normal);
+	camera_to_plane = vec3_sub(plane.origin, ray.origin);
+	t = vec3_dot(camera_to_plane, plane.normal) / vec3_dot(ray.direction,
+			plane.normal);
 	if (!interval_contains(range, t))
-		return false;
-
-	*rec = (t_hit_record) {
-		.point = ray_at(ray, t),
-		.t = t,
-		.normal = plane.normal,
-		.object = NULL,
-	};
-
-	if (vec3_dot(ray.direction, plane.normal) > 0.0)
-		rec->normal = vec3_negate(rec->normal);
-
+		return (false);
+	*rec = construct_hit_record(t, ray, plane);
 	return (true);
 }
 
-static bool parallel_ray_is_coplanar(t_plane plane, t_ray ray) {
-	const t_vec3 ray_to_plane = vec3_sub(ray.origin, plane.origin);
+static t_hit_record	construct_hit_record(double t, t_ray ray, t_plane plane)
+{
+	t_point3	hit;
 
-	return vec3_dot(plane.normal, ray_to_plane) == 0.0;
+	hit = ray_at(ray, t);
+	if (vec3_dot(ray.direction, plane.normal) > 0.0)
+	{
+		plane.normal = vec3_negate(plane.normal);
+	}
+	return ((t_hit_record){.point = hit, .t = t, .normal = plane.normal,
+		.object = NULL});
 }
 
-static bool inside_of_plane(t_plane plane, t_ray ray, t_hit_record *out)
+static bool	parallel_ray_is_coplanar(t_plane plane, t_ray ray)
 {
-	*out = (t_hit_record) {
+	const t_vec3	ray_to_plane = vec3_sub(ray.origin, plane.origin);
+
+	return (vec3_dot(plane.normal, ray_to_plane) == 0.0);
+}
+
+static bool	inside_of_plane(t_plane plane, t_ray ray, t_hit_record *out)
+{
+	*out = (t_hit_record){
 		.point = ray.origin,
 		.t = 0.0,
 		.normal = plane.normal,
