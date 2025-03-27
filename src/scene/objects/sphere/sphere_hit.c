daabe8e5d7beab0a69677c860d465a6561ab010c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-27 19:52:49 +0100

    format

--- src/scene/objects/sphere/sphere_hit.c
+++ src/scene/objects/sphere/sphere_hit.c
@@ -1,75 +1,84 @@
-#include "./t_sphere.h"
-#include "../t_hittable.h"
-#include "math/t_interval/t_interval.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere_hit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 19:43:07 by poss              #+#    #+#             */
+/*   Updated: 2025/03/27 19:52:38 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "../t_hittable.h"
+#include "./t_sphere.h"
+#include "math/t_interval/t_interval.h"
 #include <stddef.h>
 
-bool sphere_hit(t_sphere sphere, t_interval range, t_ray ray, t_hit_record *rec) {
-    t_vec3 o_c = vec3_sub(sphere.origin, ray.origin);
-
-    double a = vec3_dot(ray.direction, ray.direction);
-    double b = -2.0 * vec3_dot(ray.direction, o_c);
-    double c = vec3_dot(o_c, o_c) - sphere.radius * sphere.radius;
-
-    double delta = b * b - 4.0 * a * c;
-
-    if (delta < 0.0) {
-        return false;
-    }
-
-    double x0 = (-b - sqrt(delta)) / (2.0 * a);
-    double x1 = (-b + sqrt(delta)) / (2.0 * a);
-
-    if (interval_contains(range, x0)) {
-        t_point3 hit = ray_at(ray, x0);
-        *rec = (t_hit_record) {
-            .point = hit,
-            .t = x0,
-            .normal = vec3_normalize(vec3_sub(hit, sphere.origin)),
-        };
-        return true;
-    }
-
-    if (interval_contains(range, x1)) {
-        t_point3 hit = ray_at(ray, x1);
-        *rec = (t_hit_record) {
-            .point = hit,
-            .t = x1,
-            .normal = vec3_normalize(vec3_sub(hit, sphere.origin)),
-        };
-        return true;
-    }
-
-    return false;
-}
-/*
-    let o_c = self.origin - ray.origin;
-
-    let a = ray.direction.dot(ray.direction);
-    let b = -2.0 * ray.direction .dot (o_c);
-    let c = (o_c).dot(o_c) - self.radius * self.radius;
-
-    let discriminant = (b * b) - 4.0 * a * c;
-
-    if discriminant < 0.0 {
-        return None;
-    }
-    let x0 = (-b - discriminant.sqrt()) / (2.0*a);
-    let x1 = (-b + discriminant.sqrt()) / (2.0*a);
-    let possible_hit = if range.contains(&x0) {
-        Some(x0)
-} else if range.contains(&x1) {
-        Some(x1)
-} else {
-    None
+struct	s_sph_hit_ctx
+{
+	double	a;
+	double	b;
+	double	c;
 };
-    possible_hit.map(|t| {
-            let intersection = ray.at(t);
-            let mut normal = (intersection - self.origin).normalized();
-            let from_within = (ray.direction.dot(normal) > 0.0);
-            if from_within {
-                    normal = -normal;
-            };
-            HitRecord { t, normal, from_within, at: intersection }
-    })
-    */
+typedef struct s_sph_hit_ctx	t_sph_hit_ctx;
+
+static t_sph_hit_ctx	compute(t_sphere sph, t_ray ray)
+{
+	t_vec3	o_c;
+	double	a;
+	double	b;
+	double	c;
+
+	o_c = vec3_sub(sph.origin, ray.origin);
+	a = vec3_dot(ray.direction, ray.direction);
+	b = -2.0 * vec3_dot(ray.direction, o_c);
+	c = vec3_dot(o_c, o_c) - sph.radius * sph.radius;
+	return ((t_sph_hit_ctx){.a = a, .b = b, .c = c});
+}
+
+static t_hit_record	construct_hit_record(double t, t_ray ray, t_sphere sph)
+{
+	t_hit_record	out;
+	t_vec3			hit;
+	t_vec3			normal;
+
+	hit = ray_at(ray, t);
+	normal = vec3_normalize(vec3_sub(hit, sph.origin));
+	if (vec3_dot(ray.origin, normal) > 0.0)
+	{
+		normal = vec3_negate(normal);
+	}
+	return ((t_hit_record){.point = hit, .t = t, .normal = normal,
+		.object = NULL});
+}
+
+bool	sphere_hit(t_sphere sphere, t_interval range, t_ray ray,
+		t_hit_record *rec)
+{
+	t_sph_hit_ctx	ctx;
+	double			delta;
+	double			x0;
+	double			x1;
+
+	ctx = compute(sphere, ray);
+	delta = ctx.b * ctx.b - 4.0 * ctx.a * ctx.c;
+	if (delta < 0.0)
+	{
+		return (false);
+	}
+	delta = sqrt(delta);
+	x0 = (-ctx.b - delta) / (2.0 * ctx.a);
+	x1 = (-ctx.b + delta) / (2.0 * ctx.a);
+	if (interval_contains(range, x0))
+	{
+		*rec = construct_hit_record(x0, ray, sphere);
+		return (true);
+	}
+	if (interval_contains(range, x1))
+	{
+		*rec = construct_hit_record(x1, ray, sphere);
+		return (true);
+	}
+	return (false);
+}
