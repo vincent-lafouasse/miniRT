55abd5bed1814bb5b5ef74451e6fdc05fc066090
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-28 14:45:56 +0100

    implement disk_hit using plane_hit

--- src/scene/objects/cylinder/cylinder_cap_hit.c
+++ src/scene/objects/cylinder/cylinder_cap_hit.c
@@ -9,8 +9,27 @@ struct s_disk {
 	t_point3 center;
 	t_vec3 normal;
 	double radius;
+	t_rgb color;
 };
 typedef struct s_disk t_disk;
+
+bool	plane_hit(t_plane plane, t_interval range, t_ray ray, t_hit_record *rec);
+
+bool	disk_hit(t_disk disk, t_interval range, t_ray ray, t_hit_record *rec) {
+	t_plane parent_plane = (t_plane){.origin=disk.center, .normal=disk.normal, .color=disk.color};
+	t_hit_record record;
+
+	if (!plane_hit(parent_plane, range, ray, &record))
+		return false;
+	
+	t_vec3 op = vec3_sub(record.point, disk.center);
+	if (vec3_dot(op, op) > disk.radius * disk.radius)
+		return false;
+	if (vec3_dot(ray.direction, record.normal) > 0.0)
+		record.normal = vec3_negate(record.normal);
+	*rec = record;
+	return true;
+}
 
 void get_endcaps(t_cylinder cy, t_disk* cap1, t_disk* cap2)
 {
@@ -20,12 +39,14 @@ void get_endcaps(t_cylinder cy, t_disk* cap1, t_disk* cap2)
 		.center = vec3_add(cy.point, shift),
 		.normal = cy.axis,
 		.radius = cy.radius,
+		.color = cy.color,
 	};
 
 	*cap2 = (t_disk){
 		.center = vec3_sub(cy.point, shift),
 		.normal = cy.axis,
 		.radius = cy.radius,
+		.color = cy.color,
 	};
 }
 
