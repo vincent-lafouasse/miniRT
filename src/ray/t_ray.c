f059241d3ce010b29103eb97aeb3737b129b8c7f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-29 20:59:41 +0100

    format

--- src/ray/t_ray.c
+++ src/ray/t_ray.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_ray.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 20:59:23 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 20:59:30 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "t_ray.h"
 
-t_ray ray_new(t_point3 origin, t_vec3 direction) {
-    return (t_ray){origin, direction};
+t_ray	ray_new(t_point3 origin, t_vec3 direction)
+{
+	return ((t_ray){origin, direction});
 }
 
-t_point3 ray_at(t_ray ray, double t) {
-    return vec3_add(ray.origin, vec3_mul(t, ray.direction));
+t_point3	ray_at(t_ray ray, double t)
+{
+	return (vec3_add(ray.origin, vec3_mul(t, ray.direction)));
 }
--- src/ray/t_ray.h
+++ src/ray/t_ray.h
@@ -1,14 +1,27 @@
-#ifndef RAY_H
-#define RAY_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_ray.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 20:58:27 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 20:59:16 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "math/t_vec3/t_vec3.h"
+#ifndef T_RAY_H
+# define T_RAY_H
 
-typedef struct s_ray {
-    t_point3 origin;
-    t_vec3 direction;
-} t_ray;
+# include "math/t_vec3/t_vec3.h"
 
-t_ray ray_new(t_point3 origin, t_vec3 direction);
-t_point3 ray_at(t_ray ray, double t);
+typedef struct s_ray
+{
+	t_point3	origin;
+	t_vec3		direction;
+}				t_ray;
+
+t_ray		ray_new(t_point3 origin, t_vec3 direction);
+t_point3	ray_at(t_ray ray, double t);
 
 #endif
