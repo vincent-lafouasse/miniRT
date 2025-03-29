f24ce4675288bf13f40ad1983eb32ea3f9f80aa3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-29 19:35:31 +0100

    format

--- src/scene/objects/plane/t_plane.c
+++ src/scene/objects/plane/t_plane.c
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_plane.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 19:34:29 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 19:34:30 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./t_plane.h"
-
 #include "math/double/double.h"
-
 #include <stdio.h>
 
-static void report_non_unit_normal(void)
+static void	report_non_unit_normal(void)
 {
 	printf("plane_new: warning: plane normal vector is not normalized\n");
 }
 
-t_error plane_new(t_point3 origin, t_vec3 normal, t_rgb color, t_plane *out)
+t_error	plane_new(t_point3 origin, t_vec3 normal, t_rgb color, t_plane *out)
 {
 	if (double_eq(vec3_length(normal), 0.0))
 		return (E_UNEXPECTED_NULL_VECTOR);
--- src/scene/objects/plane/t_plane.h
+++ src/scene/objects/plane/t_plane.h
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_plane.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 19:34:51 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 19:35:15 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef T_PLANE_H
-#define T_PLANE_H
+# define T_PLANE_H
 
-#include "error/t_error.h"
+# include "error/t_error.h"
+# include "math/t_rgb/t_rgb.h"
+# include "math/t_vec3/t_vec3.h"
 
-#include "math/t_vec3/t_vec3.h"
-#include "math/t_rgb/t_rgb.h"
-
-struct s_plane {
-	t_point3 origin;
-	t_vec3 normal;
-	t_rgb color;
+struct	s_plane
+{
+	t_point3	origin;
+	t_vec3		normal;
+	t_rgb		color;
 };
-typedef struct s_plane t_plane;
+typedef struct s_plane	t_plane;
 
-t_error plane_new(t_point3 origin, t_vec3 normal, t_rgb color, t_plane *out);
-//t_hit_record plane_hit(t_camera cam, t_ray ray);
+t_error		plane_new(t_point3 origin, t_vec3 normal, t_rgb color,
+				t_plane *out);
 
 #endif // T_PLANE_H
