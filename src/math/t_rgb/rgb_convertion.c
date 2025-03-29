04a7237aa941fd3eb10c0d298fba5c909e821f83
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-30 00:26:09 +0100

    format

--- src/math/t_rgb/rgb_convertion.c
+++ src/math/t_rgb/rgb_convertion.c
@@ -14,32 +14,37 @@
 
 t_rgb	rgb_from_bytes(const uint8_t color[3])
 {
-	return rgb_from_elements(color[0], color[1], color[2]);
+	return (rgb_from_elements(color[0], color[1], color[2]));
 }
 
 t_rgb	rgb_from_elements(uint8_t r, uint8_t g, uint8_t b)
 {
-	double r_double;
-	double g_double;
-	double b_double;
+	double	r_double;
+	double	g_double;
+	double	b_double;
 
 	r_double = (double)r / 256.0;
 	g_double = (double)g / 256.0;
 	b_double = (double)b / 256.0;
-	return vec3_new(r_double, g_double, b_double);
+	return (vec3_new(r_double, g_double, b_double));
 }
 
-static uint8_t clamp_and_cast_float(double value) {
-    if (value >= 255.0) {
-        return 255;
-    } else if (value < 0.0) {
-        return 0;
-    }
-    return (uint8_t)value;
+static uint8_t	clamp_and_cast_float(double value)
+{
+	if (value >= 255.0)
+	{
+		return (255);
+	}
+	else if (value < 0.0)
+	{
+		return (0);
+	}
+	return ((uint8_t)value);
 }
 
-uint32_t	rgb_to_bytes(t_rgb self) {
-    return (clamp_and_cast_float(self.x * 256.0) << 16)
-            |(clamp_and_cast_float(self.y * 256.0) << 8)
-            | clamp_and_cast_float(self.z * 256.0);
+uint32_t	rgb_to_bytes(t_rgb self)
+{
+	return (clamp_and_cast_float(self.x
+			* 256.0) << 16) | (clamp_and_cast_float(self.y
+			* 256.0) << 8) | clamp_and_cast_float(self.z * 256.0);
 }
--- src/math/t_rgb/t_rgb.h
+++ src/math/t_rgb/t_rgb.h
@@ -6,16 +6,15 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 21:21:50 by poss              #+#    #+#             */
-/*   Updated: 2025/03/21 17:07:10 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/30 00:26:05 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_RGB_H
 # define T_RGB_H
 
-#include "math/t_vec3/t_vec3.h"
-
-#include <stdint.h>
+# include "math/t_vec3/t_vec3.h"
+# include <stdint.h>
 
 typedef struct s_vec3	t_rgb;
 
@@ -23,12 +22,14 @@ t_rgb		rgb_from_bytes(const uint8_t color[3]);
 t_rgb		rgb_from_elements(uint8_t r, uint8_t g, uint8_t b);
 uint32_t	rgb_to_bytes(t_rgb self);
 
-static inline t_rgb rgb_white(void) {
-	return (t_rgb){.x = 1.0, .y = 1.0, .z = 1.0};
+static inline t_rgb	rgb_white(void)
+{
+	return ((t_rgb){.x = 1.0, .y = 1.0, .z = 1.0});
 }
 
-static inline t_rgb rgb_black(void) {
-	return (t_rgb){.x = 0.0, .y = 0.0, .z = 0.0};
+static inline t_rgb	rgb_black(void)
+{
+	return ((t_rgb){.x = 0.0, .y = 0.0, .z = 0.0});
 }
 
 #endif
