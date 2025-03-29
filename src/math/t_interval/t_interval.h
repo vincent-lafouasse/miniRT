8aa4c09fe2205480a3578668027b82b27d936294
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-30 00:24:37 +0100

    format

--- src/math/t_interval/t_interval.h
+++ src/math/t_interval/t_interval.h
@@ -6,26 +6,27 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 19:11:20 by poss              #+#    #+#             */
-/*   Updated: 2025/03/17 19:39:02 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/30 00:24:11 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_INTERVAL_H
-#define T_INTERVAL_H
+# define T_INTERVAL_H
 
-#include <math.h>
-#include <stdbool.h>
+# include <math.h>
+# include <stdbool.h>
 
-struct s_interval {
-    double min;
-    double max;
+struct	s_interval
+{
+	double	min;
+	double	max;
 };
-typedef struct s_interval t_interval;
+typedef struct s_interval	t_interval;
 
-t_interval interval_new(double min, double max);
-double interval_size(t_interval);
-bool interval_contains(t_interval, double);
-bool interval_surrounds(t_interval, double);
-t_interval interval_R_plus(void);
+t_interval	interval_new(double min, double max);
+double		interval_size(t_interval i);
+bool		interval_contains(t_interval i, double x);
+bool		interval_surrounds(t_interval i, double x);
+t_interval	interval_R_plus(void);
 
-#endif  // T_INTERVAL_H
+#endif // T_INTERVAL_H
