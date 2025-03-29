636ac416aef8240d05f6be3450ac5686f532a06d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-29 21:02:49 +0100

    format

--- src/render/renderer_init.c
+++ src/render/renderer_init.c
@@ -1,48 +1,63 @@
-#include "t_renderer.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   renderer_init.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 21:01:16 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 21:01:17 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "error/t_error.h"
-
+#include "t_renderer.h"
 #include <mlx.h>
 #include <stddef.h>
 
-t_error renderer_init_with_exit_hooks(size_t width, double aspect_ratio, t_renderer *out)
+t_error	renderer_init_with_exit_hooks(size_t width, double aspect_ratio,
+		t_renderer *out)
 {
-    t_error err = renderer_init(width, aspect_ratio, out);
-    if (err == NO_ERROR) {
-        renderer_set_exit_loop_on_esc(out);
-        renderer_set_exit_loop_on_cross(out);
-    }
+	t_error	err;
 
-    return (err);
+	err = renderer_init(width, aspect_ratio, out);
+	if (err == NO_ERROR)
+	{
+		renderer_set_exit_loop_on_esc(out);
+		renderer_set_exit_loop_on_cross(out);
+	}
+	return (err);
 }
 
-t_error renderer_init(size_t width, double aspect_ratio, t_renderer *out) {
-    const size_t height = (double)width / aspect_ratio;
+t_error	renderer_init(size_t width, double aspect_ratio, t_renderer *out)
+{
+	const size_t	height = (double)width / aspect_ratio;
 
-    return renderer_from_dimensions(width, height, out);
+	return (renderer_from_dimensions(width, height, out));
 }
 
-t_error renderer_from_dimensions(size_t width, size_t height, t_renderer *out) {
-    t_renderer r;
+t_error	renderer_from_dimensions(size_t width, size_t height, t_renderer *out)
+{
+	t_renderer	r;
 
-    r = (t_renderer){.height = height, .width = width, 0};
-    r.mlx = mlx_init();
-    if (!r.mlx)
-        return (E_MLX_INIT);
-    r.window = mlx_new_window(r.mlx, width, r.height, "miniRT");
-    if (!r.window)
-    {
-        renderer_destroy(&r);
-        return (E_MLX_NEW_WINDOW);
-    }
-    r.img = mlx_new_image(r.mlx, width, r.height);
-    if (!r.img)
-    {
-        renderer_destroy(&r);
-        return (E_MLX_NEW_IMAGE);
-    }
-    r.addr = mlx_get_data_addr(r.img, &r.bits_per_pixel,
-                                   &r.line_length, &r.endian);
-    *out = r;
-    return (NO_ERROR);
+	r = (t_renderer){.height = height, .width = width, 0};
+	r.mlx = mlx_init();
+	if (!r.mlx)
+		return (E_MLX_INIT);
+	r.window = mlx_new_window(r.mlx, width, r.height, "miniRT");
+	if (!r.window)
+	{
+		renderer_destroy(&r);
+		return (E_MLX_NEW_WINDOW);
+	}
+	r.img = mlx_new_image(r.mlx, width, r.height);
+	if (!r.img)
+	{
+		renderer_destroy(&r);
+		return (E_MLX_NEW_IMAGE);
+	}
+	r.addr = mlx_get_data_addr(r.img, &r.bits_per_pixel, &r.line_length,
+			&r.endian);
+	*out = r;
+	return (NO_ERROR);
 }
--- src/render/t_renderer.h
+++ src/render/t_renderer.h
@@ -1,36 +1,52 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_renderer.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 21:02:42 by poss              #+#    #+#             */
+/*   Updated: 2025/03/29 21:02:45 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "error/t_error.h"
+#ifndef T_RENDERER_H
+# define T_RENDERER_H
 
-#include <stddef.h>
-#include <stdint.h>
+# include "error/t_error.h"
+# include <stddef.h>
+# include <stdint.h>
 
-typedef struct s_renderer t_renderer;
-struct s_renderer {
-    void* mlx;
-    void* window;
-    void* img;
-    char* addr;
-    size_t height;
-    size_t width;
-    int bits_per_pixel;
-    int line_length;
-    int endian;
+typedef struct s_renderer	t_renderer;
+struct	s_renderer
+{
+	void	*mlx;
+	void	*window;
+	void	*img;
+	char	*addr;
+	size_t	height;
+	size_t	width;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
 };
 
-t_error renderer_init(size_t width, double aspect_ratio, t_renderer *out);
-t_error renderer_init_with_exit_hooks(size_t width, double aspect_ratio, t_renderer *out);
-t_error renderer_from_dimensions(size_t width, size_t height, t_renderer *out);
+t_error	renderer_init(size_t width, double aspect_ratio,
+			t_renderer *out);
+t_error	renderer_init_with_exit_hooks(size_t width,
+			double aspect_ratio, t_renderer *out);
+t_error	renderer_from_dimensions(size_t width,
+			size_t height, t_renderer *out);
 
-void renderer_put_pixel(t_renderer* self,
-                               size_t x,
-                               size_t y,
-                               uint32_t color);
-void renderer_flush_pixels(t_renderer* self);
+void	renderer_put_pixel(t_renderer *self, size_t x,
+			size_t y, uint32_t color);
+void	renderer_flush_pixels(t_renderer *self);
 
-void renderer_set_exit_loop_on_esc(t_renderer* self);
-void renderer_set_exit_loop_on_cross(t_renderer* self);
+void	renderer_set_exit_loop_on_esc(t_renderer *self);
+void	renderer_set_exit_loop_on_cross(t_renderer *self);
 
-void renderer_enter_loop(t_renderer *self);
+void	renderer_enter_loop(t_renderer *self);
 
-void renderer_destroy(t_renderer *self);
+void	renderer_destroy(t_renderer *self);
+
+#endif
