230133f2b2620582ee96731ebc9340a4685091e5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-30 00:43:38 +0100

    format

--- src/error/error_repr.c
+++ src/error/error_repr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 20:54:19 by poss              #+#    #+#             */
-/*   Updated: 2025/02/08 20:56:22 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/30 00:43:34 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,50 +42,58 @@
 
 #define UNKNOWN_VARIANT_REPR "Unknown error variant"
 
-const char* error_repr(t_error err) {
-    if (err == NO_ERROR)
-        return NO_ERROR_REPR;
-    if (err == E_LINE_EMPTY)
-        return (LINE_EMPTY_REPR);
-    if (err == E_UNKNOWN_ELEMENT)
-        return (UNKNOWN_ELEMENT_REPR);
-    if (err == E_MALFORMATTED_ELEMENT)
-        return (MALFORMATTED_ELEMENT_REPR);
-    if (err == E_MALFORMATTED_COLOR)
-        return (MALFORMATTED_COLOR_REPR);
-    if (err == E_MALFORMATTED_VECTOR)
-        return (MALFORMATTED_VECTOR_REPR);
-    if (err == E_INVALID_NUMBER)
-        return (INVALID_NUMBER_REPR);
-    if (err == E_MUST_HAVE_ONE_CAMERA)
-        return (MUST_HAVE_ONE_CAMERA_REPR);
-    if (err == E_MUST_HAVE_ONE_AMBIENT_LIGHT)
-        return (MUST_HAVE_ONE_AMBIENT_LIGHT_REPR);
-    if (err == E_BAD_MAP_SUFFIX)
-        return (BAD_MAP_SUFFIX_REPR);
-    if (err == E_NO_POINT_LIGHT)
-        return (NO_POINT_LIGHT_REPR);
-    if (err == E_OUT_OF_RANGE)
-        return (OUT_OF_RANGE_REPR);
-    if (err == E_UNEXPECTED_NULL_VECTOR)
-        return (UNEXPECTED_NULL_VECTOR_REPR);
-    if (err == E_BAD_PROGRAM_USAGE)
-        return (BAD_PROGRAM_USAGE_REPR);
-    if (err == E_FLAT_TRIANGLE)
-        return (FLAT_TRIANGLE_REPR);
-    if (err == E_OOM)
-        return (OOM_REPR);
-    if (err == E_MLX_INIT)
-        return (MLX_INIT_REPR);
-    if (err == E_MLX_NEW_WINDOW)
-        return (MLX_NEW_WINDOW_REPR);
-    if (err == E_MLX_NEW_IMAGE)
-        return (MLX_NEW_IMAGE_REPR);
-    if (err == E_READ)
-        return (READ_REPR);
-    if (err == E_OPEN)
-        return (OPEN_REPR);
-    if (err == E_UNREACHABLE)
-        return (UNREACHABLE_REPR);
-    return (UNKNOWN_VARIANT_REPR);
+static const char	*error_repr_continued(t_error err);
+
+const char	*error_repr(t_error err)
+{
+	if (err == NO_ERROR)
+		return (NO_ERROR_REPR);
+	if (err == E_LINE_EMPTY)
+		return (LINE_EMPTY_REPR);
+	if (err == E_UNKNOWN_ELEMENT)
+		return (UNKNOWN_ELEMENT_REPR);
+	if (err == E_MALFORMATTED_ELEMENT)
+		return (MALFORMATTED_ELEMENT_REPR);
+	if (err == E_MALFORMATTED_COLOR)
+		return (MALFORMATTED_COLOR_REPR);
+	if (err == E_MALFORMATTED_VECTOR)
+		return (MALFORMATTED_VECTOR_REPR);
+	if (err == E_INVALID_NUMBER)
+		return (INVALID_NUMBER_REPR);
+	if (err == E_MUST_HAVE_ONE_CAMERA)
+		return (MUST_HAVE_ONE_CAMERA_REPR);
+	if (err == E_MUST_HAVE_ONE_AMBIENT_LIGHT)
+		return (MUST_HAVE_ONE_AMBIENT_LIGHT_REPR);
+	if (err == E_BAD_MAP_SUFFIX)
+		return (BAD_MAP_SUFFIX_REPR);
+	if (err == E_NO_POINT_LIGHT)
+		return (NO_POINT_LIGHT_REPR);
+	if (err == E_OUT_OF_RANGE)
+		return (OUT_OF_RANGE_REPR);
+	return (error_repr_continued(err));
+}
+
+static const char	*error_repr_continued(t_error err)
+{
+	if (err == E_UNEXPECTED_NULL_VECTOR)
+		return (UNEXPECTED_NULL_VECTOR_REPR);
+	if (err == E_BAD_PROGRAM_USAGE)
+		return (BAD_PROGRAM_USAGE_REPR);
+	if (err == E_FLAT_TRIANGLE)
+		return (FLAT_TRIANGLE_REPR);
+	if (err == E_OOM)
+		return (OOM_REPR);
+	if (err == E_MLX_INIT)
+		return (MLX_INIT_REPR);
+	if (err == E_MLX_NEW_WINDOW)
+		return (MLX_NEW_WINDOW_REPR);
+	if (err == E_MLX_NEW_IMAGE)
+		return (MLX_NEW_IMAGE_REPR);
+	if (err == E_READ)
+		return (READ_REPR);
+	if (err == E_OPEN)
+		return (OPEN_REPR);
+	if (err == E_UNREACHABLE)
+		return (UNREACHABLE_REPR);
+	return (UNKNOWN_VARIANT_REPR);
 }
--- src/error/t_error.h
+++ src/error/t_error.h
@@ -6,47 +6,40 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 20:53:20 by poss              #+#    #+#             */
-/*   Updated: 2025/03/26 20:50:39 by poss             ###   ########.fr       */
+/*   Updated: 2025/03/30 00:41:58 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_ERROR_H
-#define T_ERROR_H
+# define T_ERROR_H
 
-enum e_error {
-    NO_ERROR,
-
-    // input errors
-    E_LINE_EMPTY, // non-fatal
-    E_UNKNOWN_ELEMENT,
-    E_MALFORMATTED_ELEMENT,
-    E_MALFORMATTED_COLOR,
-    E_MALFORMATTED_VECTOR,
-    E_INVALID_NUMBER,
-    E_MUST_HAVE_ONE_CAMERA,
-    E_MUST_HAVE_ONE_AMBIENT_LIGHT,
-    E_NO_POINT_LIGHT,
-    E_OUT_OF_RANGE,
-    E_UNEXPECTED_NULL_VECTOR,
-    E_BAD_PROGRAM_USAGE,
-    E_BAD_MAP_SUFFIX,
-    E_FLAT_TRIANGLE,
-
-    // library functions errors
-    E_OOM,
-    E_MLX_INIT,
-    E_MLX_NEW_WINDOW,
-    E_MLX_NEW_IMAGE,
-
-    // system call errors
-    E_OPEN,
-    E_READ,
-
-    // programming errors
-    E_UNREACHABLE,
+enum	e_error
+{
+	NO_ERROR,
+	E_LINE_EMPTY,
+	E_UNKNOWN_ELEMENT,
+	E_MALFORMATTED_ELEMENT,
+	E_MALFORMATTED_COLOR,
+	E_MALFORMATTED_VECTOR,
+	E_INVALID_NUMBER,
+	E_MUST_HAVE_ONE_CAMERA,
+	E_MUST_HAVE_ONE_AMBIENT_LIGHT,
+	E_NO_POINT_LIGHT,
+	E_OUT_OF_RANGE,
+	E_UNEXPECTED_NULL_VECTOR,
+	E_BAD_PROGRAM_USAGE,
+	E_BAD_MAP_SUFFIX,
+	E_FLAT_TRIANGLE,
+	E_OOM,
+	E_MLX_INIT,
+	E_MLX_NEW_WINDOW,
+	E_MLX_NEW_IMAGE,
+	E_OPEN,
+	E_READ,
+	E_UNREACHABLE,
 };
-typedef enum e_error t_error;
+typedef enum e_error	t_error;
 
-const char* error_repr(t_error err);
+const char	*error_repr(t_error err);
 
 #endif
