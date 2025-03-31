b262f287aefc807960be12453ef4f99c3b8dd531
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-31 17:18:32 +0200

    format

--- src/scene/t_scene.c
+++ src/scene/t_scene.c
@@ -1,14 +1,27 @@
-#include "t_scene.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_scene.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 17:18:02 by poss              #+#    #+#             */
+/*   Updated: 2025/03/31 17:18:04 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "./lights/t_point_light_array/t_point_light_array.h"
 #include "./objects/t_hittable_array/t_hittable_array.h"
+#include "t_scene.h"
 
-t_scene scene_new(t_ambient_light ambient_light, t_point_light_array *point_lights, t_hittable_array *objects)
+t_scene	scene_new(t_ambient_light ambient_light,
+		t_point_light_array *point_lights, t_hittable_array *objects)
 {
-	return ((t_scene){.ambient_light = ambient_light, .point_lights = point_lights, .objects = objects});
+	return ((t_scene){.ambient_light = ambient_light,
+		.point_lights = point_lights, .objects = objects});
 }
 
-void scene_destroy(t_scene *scene)
+void	scene_destroy(t_scene *scene)
 {
 	point_light_array_destroy(&scene->point_lights);
 	hittable_array_destroy(&scene->objects);
--- src/scene/t_scene.h
+++ src/scene/t_scene.h
@@ -1,24 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_scene.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 17:18:06 by poss              #+#    #+#             */
+/*   Updated: 2025/03/31 17:18:21 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef T_SCENE_H
 # define T_SCENE_H
 
-#include "./lights/t_ambient_light.h"
-#include "./lights/t_point_light.h"
-#include "./lights/t_point_light_array/t_point_light_array.h"
-#include "./objects/t_hittable_array/t_hittable_array.h"
+# include "./lights/t_ambient_light.h"
+# include "./lights/t_point_light.h"
+# include "./lights/t_point_light_array/t_point_light_array.h"
+# include "./objects/t_hittable_array/t_hittable_array.h"
+# include "math/t_rgb/t_rgb.h"
 
-#include "math/t_rgb/t_rgb.h"
-
-struct s_scene {
-	t_ambient_light			ambient_light;
-	t_point_light_array		*point_lights;
-	t_hittable_array		*objects;
+struct	s_scene
+{
+	t_ambient_light		ambient_light;
+	t_point_light_array	*point_lights;
+	t_hittable_array	*objects;
 };
 typedef struct s_scene	t_scene;
 
-t_scene scene_new(t_ambient_light ambient_light, t_point_light_array *point_lights, t_hittable_array *objects);
+t_scene	scene_new(t_ambient_light ambient_light,
+			t_point_light_array *point_lights,
+			t_hittable_array *objects);
 
-t_rgb scene_ray_color(const t_scene* scene, t_ray r);
+t_rgb	scene_ray_color(const t_scene *scene, t_ray r);
 
-void scene_destroy(t_scene *scene);
+void	scene_destroy(t_scene *scene);
 
 #endif // T_SCENE_H
