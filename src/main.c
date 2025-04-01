fc1010b674230647b06506521a73bd176a628404
bitquence <imbitquence@gmail.com>
2025-04-01 18:03:37 +0200

    offset render progress by one

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ void render(const t_camera* camera,
             const t_scene* scene,
             t_renderer* renderer) {
     for (size_t x = 0; x < renderer->width; x++) {
-        printf("%zu/%zu\n", x, renderer->width);
+        printf("%zu/%zu\n", x + 1, renderer->width);
         for (size_t y = 0; y < renderer->height; y++) {
             t_rgb color = camera_pixel_color(camera, scene, x, y);
             renderer_put_pixel(renderer, x, y, rgb_to_bytes(color));
