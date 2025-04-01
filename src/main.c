38f8d4e7adb9fedb2c9d9ea721be3b14b9f7e851
bitquence <imbitquence@gmail.com>
2025-04-01 17:15:19 +0200

    extract parsing logic from main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "error/t_error.h"
 #include "math/t_rgb/t_rgb.h"
 #include "parser/parse.h"
 #include "render/t_renderer.h"
@@ -26,11 +27,31 @@ void render(const t_camera* camera,
 
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+t_error parse_map(const char *filename, t_camera *cam_out, t_scene *scene_out)
+{
     t_error err;
     t_camera_specs specs;
     t_scene scene;
     t_camera camera;
+
+    err = parse_from_file(filename, &specs, &scene);
+    if (err != NO_ERROR)
+        return (err);
+    err = camera_new(specs, WIDTH, WIDTH / (double)ASPECT_RATIO, &camera);
+    if (err != NO_ERROR)
+    {
+        scene_destroy(&scene);
+        return (err);
+    }
+    *cam_out = camera;
+    *scene_out = scene;
+    return (NO_ERROR);
+}
+
+int main(int argc, char **argv) {
+    t_error err;
+    t_camera camera;
+    t_scene scene;
     t_renderer renderer;
 
     if (argc != 2)
@@ -38,17 +59,10 @@ int main(int argc, char **argv) {
         log_error(E_BAD_PROGRAM_USAGE);
         return (EXIT_FAILURE);
     }
-    err = parse_from_file(argv[1], &specs, &scene);
+    err  = parse_map(argv[1], &camera, &scene);
     if (err != NO_ERROR)
     {
         log_error(err);
-        return (EXIT_FAILURE);
-    }
-    err = camera_new(specs, WIDTH, WIDTH / (double)ASPECT_RATIO, &camera);
-    if (err != NO_ERROR)
-    {
-        log_error(err);
-        scene_destroy(&scene);
         return (EXIT_FAILURE);
     }
     err = renderer_init_with_exit_hooks(WIDTH, ASPECT_RATIO, &renderer);
