85c6eeac250e94da4c1d8e7bc930a4f6e269d4ad
bitquence <imbitquence@gmail.com>
2025-04-01 20:01:38 +0200

    replace for loop with while

--- src/camera/render_scene.c
+++ src/camera/render_scene.c
@@ -26,15 +26,21 @@ void	camera_render_scene(const t_camera *camera, const t_scene *scene,
 		t_renderer *renderer)
 {
 	t_rgb	color;
+	size_t	x;
+	size_t	y;
 
-	for (size_t x = 0; x < renderer->width; x++)
+	x = 0;
+	while (x < renderer->width)
 	{
 		printf("%zu/%zu\n", x + 1, renderer->width);
-		for (size_t y = 0; y < renderer->height; y++)
+		y = 0;
+		while (y < renderer->height)
 		{
 			color = camera_pixel_color(camera, scene, x, y);
 			renderer_put_pixel(renderer, x, y, rgb_to_bytes(color));
+			y++;
 		}
+		x++;
 	}
 	renderer_flush_pixels(renderer);
 	renderer_enter_loop(renderer);
