5d2ba538a413bc2c384bd120cd33a2f79eef2401
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-01 19:47:24 +0200

    refactor shaft_hit

--- src/scene/objects/cylinder/cylinder_shaft_hit.c
+++ src/scene/objects/cylinder/cylinder_shaft_hit.c
@@ -7,56 +7,84 @@
 
 #include <stdbool.h>
 
-bool cylinder_shaft_hit(t_cylinder cylinder, t_interval range, t_ray ray, t_hit_record *rec)
+struct s_shaft_ctx
 {
-	t_vec3 rc = vec3_sub(ray.origin, cylinder.point);
+	t_cylinder cy;
+	t_ray r;
+	t_vec3	rc;
+	t_vec3 n;
+	double n_len;
+	double d;
+};
+typedef struct s_shaft_ctx	t_shaft_ctx;
 
-	t_vec3 n = vec3_cross(ray.direction, cylinder.axis);
-	double n_len = vec3_length(n);
-	if (double_eq(n_len, 0))
+static bool construct_ctx(t_cylinder cy, t_ray r, t_shaft_ctx* ctx)
+{
+	ctx->cy = cy;
+	ctx->r = r;
+	ctx->rc = vec3_sub(r.origin, cy.point);
+	ctx->n = vec3_cross(r.direction, cy.axis);
+	ctx->n_len = vec3_length(ctx->n);
+	if (double_eq(ctx->n_len, 0))
 		return false;
-	n = vec3_div(n, n_len);
-
-	// shortest distqnce
-	double d = fabs(vec3_dot(rc, n));
-	if (d >= cylinder.radius)
+	ctx->n = vec3_div(ctx->n, ctx->n_len);
+	ctx->d = fabs(vec3_dot(ctx->rc, ctx->n));
+	if (ctx->d >= cy.radius)
 		return false;
+	return (true);
+}
 
-	t_vec3 o = vec3_cross(rc, cylinder.axis);
-	double t_midpoint = -vec3_dot(o, n) / n_len;
+struct s_shaft_hits
+{
+	double	t0;
+	double	t1;
+};
+typedef struct s_shaft_hits	t_shaft_hits;
 
-	o = vec3_cross(n, cylinder.axis);
+static void get_candidates(const t_shaft_ctx* ctx, t_shaft_hits* hits)
+{
+	t_vec3 o = vec3_cross(ctx->rc, ctx->cy.axis);
+	double t_midpoint = -vec3_dot(o, ctx->n) / ctx->n_len;
+
+	o = vec3_cross(ctx->n, ctx->cy.axis);
 	o = vec3_normalize(o);
-	double t_shift = fabs(sqrt(cylinder.radius * cylinder.radius - d * d) / vec3_dot(ray.direction, o));
+	double t_shift = fabs(sqrt(ctx->cy.radius * ctx->cy.radius - ctx->d * ctx->d) / vec3_dot(ctx->r.direction, o));
 
 	double t0 = t_midpoint - t_shift;
 	double t1 = t_midpoint + t_shift;
 
-	if (interval_contains(range, t0)) {
-		t_point3 p = ray_at(ray, t0);
-		double h = vec3_dot(cylinder.axis, vec3_sub(p, cylinder.point));
-		t_point3 projection = vec3_add(cylinder.point, vec3_mul(h, cylinder.axis));
-		t_vec3 normal = vec3_sub(p, projection);
-		if (-cylinder.height / 2 <= h && h <= cylinder.height / 2) {
-			*rec = (t_hit_record){.point = p, .t = t0, .normal = vec3_normalize(normal)};
-			if (vec3_dot(ray.direction, rec->normal) > 0)
-				rec->normal = vec3_negate(rec->normal);
-			return true;
-		}
-	}
+	hits->t0 = t0;
+	hits->t1 = t1;
+}
 
-	if (interval_contains(range, t1)) {
-		t_point3 p = ray_at(ray, t1);
-		double h = vec3_dot(cylinder.axis, vec3_sub(p, cylinder.point));
-		t_point3 projection = vec3_add(cylinder.point, vec3_mul(h, cylinder.axis));
-		t_vec3 normal = vec3_sub(p, projection);
-		if (-cylinder.height / 2 <= h && h <= cylinder.height / 2) {
-			*rec = (t_hit_record){.point = p, .t = t1, .normal = vec3_normalize(normal)};
-			if (vec3_dot(ray.direction, rec->normal) > 0)
-				rec->normal = vec3_negate(rec->normal);
-			return true;
-		}
-	}
+bool candidate_hit_is_valid(double t, t_interval range, const t_shaft_ctx *ctx, t_hit_record* hit)
+{
+	if (!interval_contains(range, t))
+		return false;
 
+	t_point3 p = ray_at(ctx->r, t);
+	double h = vec3_dot(ctx->cy.axis, vec3_sub(p, ctx->cy.point));
+	t_point3 projection = vec3_add(ctx->cy.point, vec3_mul(h, ctx->cy.axis));
+	if (-ctx->cy.height / 2 > h || h > ctx->cy.height / 2)
+		return false;
+	t_vec3 normal = vec3_sub(p, projection);
+	*hit = (t_hit_record){.point = p, .t = t, .normal = vec3_normalize(normal)};
+	if (vec3_dot(ctx->r.direction, hit->normal) > 0)
+		hit->normal = vec3_negate(hit->normal);
+	return true;
+}
+
+bool cylinder_shaft_hit(t_cylinder cylinder, t_interval range, t_ray ray, t_hit_record *rec)
+{
+	t_shaft_ctx ctx;
+	t_shaft_hits candidates;
+
+	if (!construct_ctx(cylinder, ray, &ctx))
+		return false;
+	get_candidates(&ctx, &candidates);
+	if (candidate_hit_is_valid(candidates.t0, range, &ctx, rec))
+		return true;
+	if (candidate_hit_is_valid(candidates.t1, range, &ctx, rec))
+		return true;
 	return false;
 }
