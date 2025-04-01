414b34da20cf2e1052b657871833acb74a6874a4
bitquence <imbitquence@gmail.com>
2025-04-01 18:30:58 +0200

    fix acne when plane clips spot light

--- src/scene/shading/hit_is_in_the_shadow.c
+++ src/scene/shading/hit_is_in_the_shadow.c
@@ -19,12 +19,14 @@ bool	hit_is_in_shadow(t_hit_record rec, const t_hittable_array *objects,
 	t_vec3			hit_to_light;
 	double			distance_to_light;
 	t_vec3			hit_to_light_unit;
+	double			threshold;
 	t_hit_record	_;
 
 	hit_to_light = vec3_sub(light.coordinates, rec.point);
 	distance_to_light = vec3_length(hit_to_light);
 	hit_to_light_unit = vec3_div(hit_to_light, distance_to_light);
-	return (hittable_array_hit(objects, interval_new(1024 * distance_to_light
-				* DOUBLE_EPSILON, distance_to_light),
+	threshold = 1024 * DOUBLE_EPSILON;
+	return (hittable_array_hit(objects, interval_new(threshold
+				* distance_to_light, distance_to_light - threshold),
 			(t_ray){.origin = rec.point, .direction = hit_to_light_unit}, &_));
 }
