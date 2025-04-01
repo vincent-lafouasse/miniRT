b43972b3241c019b4ff64c4f6a35d613f436f87b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-01 19:54:05 +0200

    format

--- src/scene/objects/triangle/t_triangle.h
+++ src/scene/objects/triangle/t_triangle.h
@@ -1,28 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_triangle.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 19:48:39 by poss              #+#    #+#             */
+/*   Updated: 2025/04/01 19:49:01 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef T_TRIANGLE_H
-#define T_TRIANGLE_H
+# define T_TRIANGLE_H
 
-#include "error/t_error.h"
+# include "error/t_error.h"
+# include "math/t_rgb/t_rgb.h"
+# include "math/t_vec3/t_vec3.h"
 
-#include "math/t_vec3/t_vec3.h"
-#include "math/t_rgb/t_rgb.h"
-
-struct s_triangle {
-	t_point3 a;
-	t_point3 b;
-	t_point3 c;
-	t_vec3 normal;
-	t_rgb color;
+struct	s_triangle
+{
+	t_point3	a;
+	t_point3	b;
+	t_point3	c;
+	t_vec3		normal;
+	t_rgb		color;
 };
-typedef struct s_triangle t_triangle;
+typedef struct s_triangle		t_triangle;
 
-struct s_triangle_specs {
-	t_point3 a;
-	t_point3 b;
-	t_point3 c;
-	t_rgb color;
+struct	s_triangle_specs
+{
+	t_point3	a;
+	t_point3	b;
+	t_point3	c;
+	t_rgb		color;
 };
-typedef struct s_triangle_specs t_triangle_specs;
+typedef struct s_triangle_specs	t_triangle_specs;
 
-t_error triangle_new(t_triangle_specs specs, t_triangle* out);
+t_error	triangle_new(t_triangle_specs specs,
+			t_triangle *out);
 
 #endif
--- src/scene/objects/triangle/triangle_new.c
+++ src/scene/objects/triangle/triangle_new.c
@@ -1,31 +1,57 @@
-#include "t_triangle.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   triangle_new.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 19:49:30 by poss              #+#    #+#             */
+/*   Updated: 2025/04/01 19:53:46 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "math/double/double.h"
+#include "t_triangle.h"
 
-t_error triangle_new(t_triangle_specs specs, t_triangle* out) {
+struct	s_edges
+{
+	t_vec3	e1;
+	t_vec3	e2;
+	t_vec3	e3;
+};
+typedef struct s_edges	t_edges;
 
-	t_vec3 edge1 = vec3_sub(specs.a, specs.b);
-	t_vec3 edge2 = vec3_sub(specs.a, specs.c);
-	t_vec3 edge3 = vec3_sub(specs.b, specs.c);
-
-	if (
-		double_eq(0.0, vec3_length(edge1))
-		|| double_eq(0.0, vec3_length(edge2))
-		|| double_eq(0.0, vec3_length(edge3))
-	) {
-		return E_FLAT_TRIANGLE;
+t_error	construct_edges(t_triangle_specs specs, t_edges *edges)
+{
+	edges->e1 = vec3_sub(specs.a, specs.b);
+	edges->e2 = vec3_sub(specs.a, specs.c);
+	edges->e3 = vec3_sub(specs.b, specs.c);
+	if (double_eq(0.0, vec3_length(edges->e1)) || double_eq(0.0,
+			vec3_length(edges->e2)) || double_eq(0.0, vec3_length(edges->e3)))
+	{
+		return (E_FLAT_TRIANGLE);
 	}
+	return (NO_ERROR);
+}
 
-	t_vec3 normal = vec3_cross(edge1, edge2);
-	double normal_len = vec3_length(normal);
-	if (double_eq(0.0, normal_len)) {
-		return E_FLAT_TRIANGLE;
-	}
+t_error	triangle_new(t_triangle_specs specs, t_triangle *out)
+{
+	t_edges	edges;
+	t_vec3	normal;
+	double	normal_len;
 
-	*out = (t_triangle) {
-		.a=specs.a, .b=specs.b, .c=specs.c,
+	if (construct_edges(specs, &edges) != NO_ERROR)
+		return (E_FLAT_TRIANGLE);
+	normal = vec3_cross(edges.e1, edges.e2);
+	normal_len = vec3_length(normal);
+	if (double_eq(0.0, normal_len))
+		return (E_FLAT_TRIANGLE);
+	*out = (t_triangle){
+		.a = specs.a,
+		.b = specs.b,
+		.c = specs.c,
 		.normal = vec3_div(normal, normal_len),
 		.color = specs.color,
 	};
-	return NO_ERROR;
+	return (NO_ERROR);
 }
